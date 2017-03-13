d841a93d9fac048e394d4a785d753d776061095b
logileifs <logantheragon@gmail.com>
2017-03-13 01:06:54 +0000

    added setup & deploy jobs and made build job better

---
+++ fabfile.py
@@ -0,0 +1,39 @@
+from fabric.api import *
+from fabric.contrib.project import rsync_project
+
+
+def setup(host, user, prod_name):
+	env.host_string = host
+	env.user = user
+	sudo('apt-get update')
+	sudo('apt-get install -y uwsgi nginx python-pip build-essential libssl-dev libffi-dev python-dev')
+	sudo('pip install virtualenvwrapper')
+	sudo("if [ -z ${WORKON_HOME+x} ]; then echo 'export WORKON_HOME=$HOME/.virtualenvs' >> ~/.profile; fi")
+	sudo("if [ -z ${WORKON_HOME+x} ]; then echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.profile; fi")
+	run('source .profile')
+	run('mkvirtualenv ' + prod_name)
+	put('templates/dumpit.conf', '/etc/init/', use_sudo=True)
+	put('templates/uwsgi.ini', '/home/' + user + '/', use_sudo=False)
+	sudo('chown root:root /etc/init/dumpit.conf')
+	sudo('chmod 644 /etc/init/dumpit.conf')
+	put('templates/dumpit', '/etc/nginx/sites-available/', use_sudo=True)
+	sudo('chown root:root /etc/nginx/sites-available/dumpit')
+	sudo('chmod 644 /etc/nginx/sites-available/dumpit')
+	with settings(warn_only=True):
+		sudo('ln -s /etc/nginx/sites-available/dumpit /etc/nginx/sites-enabled')
+	put('templates/bootstrap.py', '/home/' + user + '/', use_sudo=False)
+
+
+def deploy(host, user, prod_name):
+	env.host_string = host
+	env.user = user
+	rsync_project(remote_dir='/home/' + user + '/', local_dir='dist/')
+	run('workon ' + prod_name)
+	sudo('/home/' + user + '/.virtualenvs/' + prod_name + '/bin/pip install -r requirements.txt')
+	sudo('restart ' + prod_name)
+	#sudo('unzip -j dumpit.zip start.py -d .')
+
+def restart(host, user):
+	env.host_string = host
+	env.user = user
+	sudo('shutdown -r now')
--- makefile
+++ makefile
@@ -1,4 +1,14 @@
+.PHONY: clean
+
 productname="dumpit"
+host="dumpit.pw"
+user="dumpitadmin"
+
+add-public-key:
+	@ cat ~/.ssh/id_rsa.pub | ssh root@dumpit.pw "mkdir -p /home/dumpitadmin/.ssh && cat >> /home/dumpitadmin/.ssh/authorized_keys"
+
+setup:
+	@ fab setup:$(host),$(user),$(productname)
 
 unit-tests:
 	@ ~/.virtualenvs/dumpit/bin/nosetests tests/unit.py
@@ -9,5 +19,14 @@ clean:
 build:
 	@ rm -rf dist/
 	@ mkdir dist/
-	@ ( cd src/ && zip -r ../dist/$(productname).pyx * )
-	@ chmod +x dist/$(productname).pyx
+	@ pip freeze > requirements.txt
+	@ ( cd src/ && zip -r ../dist/$(productname).zip * )
+	@ chmod +x dist/$(productname).zip
+	@ echo '#!/usr/bin/env python' | cat - dist/dumpit.zip &> /dev/null
+	@ cp requirements.txt dist/
+
+deploy:
+	@ fab deploy:$(host),$(user),$(productname)
+
+restart:
+	@ fab restart:$(host),$(user)
